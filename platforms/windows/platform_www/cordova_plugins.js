c688ec0f458138a0b424564b1113e6a938b43693
David Lau <squished18@gmail.com>
2017-05-10 11:39:37 -0700

    dlau

--- platforms/windows/platform_www/cordova_plugins.js
+++ platforms/windows/platform_www/cordova_plugins.js
@@ -1,9 +1,31 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "id": "cordova-sqlite-storage.SQLiteProxy",
+        "file": "plugins/cordova-sqlite-storage/src/windows/sqlite-proxy.js",
+        "pluginId": "cordova-sqlite-storage",
+        "runs": true
+    },
+    {
+        "id": "cordova-sqlite-storage.SQLite3",
+        "file": "plugins/cordova-sqlite-storage/src/windows/SQLite3-Win-RT/SQLite3JS/js/SQLite3.js",
+        "pluginId": "cordova-sqlite-storage",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2"
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-sqlite-storage": "2.0.3"
 };
 // BOTTOM OF METADATA
 });
